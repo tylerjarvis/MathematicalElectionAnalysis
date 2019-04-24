930227255f34e7eccfd106778d95ee645069ac3e
Adam Shirey <adam.shirey@gmail.com>
2019-04-23 21:17:54 -0700

    +format files; previous parsing is format 2; adding format 4

--- Analysis/ExcelConversion.py
+++ Analysis/ExcelConversion.py
@@ -9,11 +9,6 @@ class RaceConversion(object):
     Handles the parsing of CSV copied/exported from Excel and the INSERT into a database
     CSV are assumed to be a single race spanning multiple precincts
     """
-
-    # fields in the CSV that are guaranteed and in the same place for every
-    # race.
-    RegVoterIdx, TotalVotesIdx = 1, 2
-
     def __init__(self, lines):
         self.lines = lines
 
@@ -22,14 +17,18 @@ class RaceConversion(object):
         self.Candidates : list[str] = []
         self.Results : List[tuple(str,str,int)] = []
 
-    def Parse(self):
+    def Parse2(self):
+        # fields in the CSV that are guaranteed and in the same place for every race.
+        RegVoterIdx, TotalVotesIdx = 1, 2
+
         # First cell is the race, eg "US President"
         self.Race = self.lines[0][0]
         
         # Second line is empty
 
         # Get the candidate names. These are on the same line as 'Registered Voters', etc.
-        candidates = [ (i, self.lines[2][i]) for i in range(RaceConversion.TotalVotesIdx + 1, len(self.lines[2])) if self.lines[2][i].strip() != '']
+        candidate_line = self.lines[2]
+        candidates = [ (i, candidate_line[i]) for i in range(RaceConversion.TotalVotesIdx + 1, len(candidate_line)) if candidate_line[i].strip() != '']
         self.Candidates = [name for (i,name) in candidates]
 
         # Precinct names are listed in rows starting at index 4 and each take up five rows
@@ -67,6 +66,35 @@ class RaceConversion(object):
                     print("   Provisional: %s" % counts[3])
                     continue
                 self.Results.append((candidate, precinct, total))
+
+    def Parse4(self):
+        # First cell is the race, eg "US President"
+        self.Race = self.lines[0][0]
+        
+        # Second line is candidate names
+        candidate_line = self.lines[1]
+        candidates = [ (i, candidate_line[i]) for i in range(len(candidate_line)) if candidate_line[i].strip() != '']
+        self.Candidates = [name for (i,name) in candidates]
+
+        # Precinct names are listed in rows starting at index 4 and each take up five rows
+
+        # Read from the fourth line to the last precinct; this format includes a Total, which is unnecessary
+        precincts = [(i, self.lines[i][0]) for i in range(3, len(self.lines)-1) if self.lines[i][0].strip() != '']
+        self.Precincts = [name for (i,name) in precincts]
+
+
+        for (p,precinct) in precincts:
+            for (c, candidate) in candidates:
+                # Total Votes is offset +3 from the candidate's name
+                total = self.lines[p][c+3]
+                
+                try:
+                    total = int(total)
+                except:
+                    print("Non-integer vote count value ('%s') found for race '%s', precinct '%s', and candidate '%s'" % (total, self.Race, precinct, candidate))
+                    continue
+                self.Results.append((candidate, precinct, total))
+
  
 class ExcelConversion(object):
     def __init__(self, filename:str, delimiter:str=None):
@@ -90,7 +118,7 @@ class ExcelConversion(object):
 
         self.Races : list[RaceConversion] = []
 
-    def Parse(self):
+    def Parse2(self):
         """Split up the parsed CSV into child lists and hand it off to RaceConversion
         """
         # Find the line with the race description
@@ -115,9 +143,30 @@ class ExcelConversion(object):
             # slice the list[][] and create a RaceConversion
             raceLines = [line[current_index:next_index] for line in subset]
             race = RaceConversion(raceLines)
-            race.Parse()
+            race.Parse2()
 
             self.Races.append(race)
+
+    def Parse4(self):
+        """Format 4 is one race per sheet, so the ExcelConversion simply hands off parsing to Race
+        """
+        race = RaceConversion(self.lines)
+        race.Parse4()
+        self.Races = [ race ]
+
+    def Dump(self):
+        print("Races and candidates:")
+        for race in self.Races:
+            print(" - " + race.Race)
+            for candidate in race.Candidates:
+                print("    - " + candidate)
+        print()
+
+        r = self.Races[0]
+        print("Precincts (from the first race, '%s'):" % r.Race)
+        for precinct in r.Precincts:
+            print("    " + precinct)
+        print()
 
 class ElectionDatabase(object):
     def __init__(self, filename):
@@ -169,28 +218,21 @@ class ElectionDatabase(object):
 
 
 if __name__ == '__main__':
-    filename = '2018 General Election - Box Elder Precinct-Level Results.tsv'
+    #filename = '2018 General Election - Box Elder Precinct-Level Results.tsv' # format 2
+    filename1 = '2018 General Election - Cache Precinct-Level Results - Format 1.csv'
+    #filename2 = '2018 General Election - Beaver Precinct-Level Results - Format 2.csv'
+    filename3 = '2018 General Election - Grand Precinct-Level Results - Format 3.csv'
+    filename4 = '2018 General Election - Salt Lake Precinct-Level Results - Sheet 5 - Format 4.csv'
 
     if len(sys.argv) > 1 and os.path.exists(sys.argv[1]):
         filename = sys.argv[1]
 
-    converter = ExcelConversion(filename)
-    converter.Parse()
+    converter = ExcelConversion(filename4)
+    converter.Parse4()
 
     print("Parsing completed")
+    converter.Dump()
 
-    print("Races and candidates discovered:")
-    for race in converter.Races:
-        print(" - " + race.Race)
-        for candidate in race.Candidates:
-            print("    - " + candidate)
-    print()
-
-    r = converter.Races[0]
-    print("Precincts discovered (from the first race, '%s'):" % r.Race)
-    for precinct in r.Precincts:
-        print("    " + precinct)
-    print()
 
     #db = ElectionDatabase(r'C:\Users\adams\Source\Repos\T4DataEntry\T4DataEntry\bin\Debug\data.sqlite')
     #db.CommitParsedResults(converter, '2018 General Election')
